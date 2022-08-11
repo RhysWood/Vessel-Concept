8971ec12a082a3b3e8f122421bd07e2ec155d8af
RhysWood <rhyswoody@gmail.com>
2022-08-10 17:59:20 -0700

    add inventory seeds

--- express-back-end/db/seeds/02_businesses.sql
+++ express-back-end/db/seeds/02_businesses.sql
@@ -2,4 +2,4 @@ INSERT INTO businesses (name, address, main_contact, CRS) VALUES
 ('Main St Brewing', '123 Main St', 1, 'Brad Hawthshaw'),
 ('Strathcona Brewing', '123 East Hastings', 2, 'Erin Boyle'),
 ('Brassneck Brewing', '123 West Hastings', 3, 'Erin Boyle'),
-('Bucktown Brewery', '123 East Hastings', 4, 'Erin Boyle');
+('Studio Brewing', '123 Colburn Street', 4, 'Erin Boyle');
