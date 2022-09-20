b56f6986e2634c29ed96b2418dca296f9942189d
RhysWood <rhyswoody@gmail.com>
2022-09-20 11:09:15 -0700

    add conditional welcome text rendering

--- react-front-end/src/components/Home.jsx
+++ react-front-end/src/components/Home.jsx
@@ -2,17 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
 import '../styles/Home.scss';
+import profileState from './atoms';
+import { useRecoilState } from 'recoil';
 
 export default function Home() {
-    return(
-        <div className="parent">
-            <div classname="welcome">
-                <h1>
-                    <p class="line-1 anim-typewriter">Welcome to Vessel Packaging</p>
+    const [profile] = useRecoilState(profileState);
+
+    const conditionalRender = () => {
+        if (profile.length === 0) {
+            return (
+                <>
+                      <h1>
+                    <p class="line-1 anim-typewriter">Welcome to Vessel</p>
                 </h1>
                 <Link to="login">
                     <Button>Login</Button>
                 </Link>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <p class="line-1 anim-typewriter2">{`Hello, ${profile.first_name}, ready to order?`}</p>
+                    <Button>Order</Button>
+                </>
+            )
+        }
+    }
+    return(
+        <div className="parent">
+            <div classname="welcome">
+              {conditionalRender()}
             </div>
         </div>
     )
